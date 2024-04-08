81c723f3d31746525ddd451191c5e05731d5f064
Maksym Rekalo <maksim.monor@gmail.com>
2024-04-08 12:40:50 +0300

    2

--- src/functions/sendRequest.js
+++ src/functions/sendRequest.js
@@ -5,21 +5,20 @@ const apiPath = 'http://localhost:3002/api/select'
 export default async function sendRequest (method = 'get', req_data = {}, request_num = '1') {
     switch (method) {
         case 'get':
-            let {data_get} = await axios.get(apiPath + request_num);
-            return data_get;
+            let response_1 = await axios.get(apiPath + request_num);
+            return response_1.data;
         case 'post':
             try {
-                let {data_post} = await axios.post(apiPath + request_num, {...req_data});
-                return data_post;
+                let response_2 = await axios.post(apiPath + request_num, {...req_data});
+                return response_2.data;
             } catch (error) {
                 console.log(error);
             }
-            // let data_post = [{'repair_id': 1, 'sc_id': 2}]
         case 'delete':
-            let {data_delete} = await axios.delete(apiPath + request_num);
-            return data_delete;
-        case 'update':
-            let {data_update} = await axios.put(apiPath + request_num, {...req_data});
-            return data_update
+            let response_3 = await axios.delete(apiPath + request_num);
+            return response_3.data;
+        case 'patch':
+            let response_4 = await axios.patch(apiPath + request_num, {...req_data});
+            return response_4.data
     }
 }
